562adaf599a72c9f47939bcb08f6855d3bd5ed5d
AravindNaik1363 <98738331+AravindNaik1363@users.noreply.github.com>
2022-02-27 19:00:02 +0530

    comma operator examples

--- comma operator.c
+++ comma operator.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-int func(int,int);
+int func(int,int);//function prototype
 
-int func(int x,int y)
+int func(int x,int y)//function definition
 {
     return (x+y);
 }
@@ -10,13 +10,12 @@ int func(int x,int y)
 int main()
 {
     int a=5,b=10,c;
-    c=func(a,b);
+    c=func(a,b);//function calling
     printf("c value is %d",c);
     c=1,2,3;
     printf("\nc value is:%d",c);
     c=(1,2,3);
     printf("\nc value is:%d",c);
-    
-    
+    printf("\nfunc(2,4)+(1,4) output is %d",func(2,4)+(1,4));
     return 0;
 }
