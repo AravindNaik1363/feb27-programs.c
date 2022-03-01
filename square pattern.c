ce93896a0d405ab54f7168c21ccc0b29853fca97
AravindNaik1363 <98738331+AravindNaik1363@users.noreply.github.com>
2022-03-01 17:23:47 +0530

    pattern7

--- square pattern.c
+++ square pattern.c
@@ -3,9 +3,9 @@
 int main()
 {
     int i,j;
-    for(i=1;i<=3;i++)
+    for(i=1;i<=5;i++)
     {
-        for(j=1;j<=3;j++)
+        for(j=1;j<=5;j++)
         {
             printf("*");
         }
